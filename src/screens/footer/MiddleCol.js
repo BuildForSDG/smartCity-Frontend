0bb9d8174a1b9a4194677a86ae9ef3b8c9cfff7a
Emuohwo <warriisaac@outlook.com>
2020-06-09 09:04:32 -0700

    Add footer to the project

---
+++ src/screens/footer/MiddleCol.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MiddleColumn = () => (
+    <>
+        <h4>Quick Links</h4>
+        <div>
+            <p>
+            <a style={{color: '#f7b731'}} href="#">help</a>
+            </p>
+        </div>
+    </>
+);
+
+export default MiddleColumn;
