e341ec9fe05f23deef1ffc77ad481ca265dd2d73
Sunny Cletus <scletus@gmail.com>
2020-06-23 14:30:07 +0100

    auth services

--- src/config/system.js
+++ src/config/system.js
@@ -3,7 +3,7 @@ const system = {
     serverBaseUrl: 'https://nameless-reaches-59547.herokuapp.com',
     imageBaseUrl: 'https://nameless-reaches-59547.herokuapp.com/products/images',
     ArtisanImageUrl: 'https://nameless-reaches-59547.herokuapp.com/artisans/images',
-    stripeToken: 'pk_test_NcwpaplBCuTL6I0THD44heRe'
+    userBaseUrl: 'https://nameless-reaches-59547.herokuapp.com/users'
 };
 
 export default system;
---
+++ src/services/userServices.js
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import EventEmitter from '../utils/EventEmitter';
+import systemConfig from '../config/system';
+
+class userService extends EventEmitter {
+  constructor() {
+    super();
+
+    this.setDefaults();
+  }
+
+  setDefaults() {
+    axios.defaults.headers.common['Content-Type'] = 'application/json';
+    axios.defaults.headers.common['Accept'] = 'application/json';
+  }
+
+  postUser({type, user}) {
+    return () => {
+        return new Promise((resolve, reject) => {
+          axios
+            .post(systemConfig.userBaseUrl + `/${type}`, {
+              ...user
+            })
+            .then((response) => {
+              resolve(response.data);
+            })
+            .catch((error) => {
+              reject(error.response.data);
+            });
+        });
+      }
+  }
+
+
+  getUser({type}) {
+    const url = type ? systemConfig.userBaseUrl + `/${type}` : systemConfig.userBaseUrl
+    return () => {
+        return new Promise((resolve, reject) => {
+          axios
+            .get(url)
+            .then((response) => {
+              resolve(response.data);
+            })
+            .catch((error) => {
+              reject(error.response.data);
+            });
+        });
+      }
+  }
+
+  /* login(user) {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(systemConfig.userBaseUrl + '/login', {
+          ...user
+        })
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error.response.data);
+        });
+    });
+  } */
+
+}
+
+const instance = new userService();
+
+export default instance;
